1af37ba9a345f4456749768cfc9386e9d4f652d5
Vladimir <jaba0192837465@gmail.com>
2024-08-28 01:32:32 +0300

    gitignore

--- build/main.d
+++
@@ -1,8 +0,0 @@
-build/main.o: src/main.c src/streams/streams.h \
- src/streams/../utils/utils.h src/zipping/zipping.h \
- src/zipping/../utils/utils.h src/utils/utils.h
-src/streams/streams.h:
-src/streams/../utils/utils.h:
-src/zipping/zipping.h:
-src/zipping/../utils/utils.h:
-src/utils/utils.h:
